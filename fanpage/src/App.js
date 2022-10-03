9cc86f22874a0cb0093f3083e26d8a02952f1f51
Juan Almeida <juanreboucas@gmail.com>
2022-10-03 16:50:19 -0300

    criando home da index

--- fanpage/src/App.js
+++ fanpage/src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="#" className="logo">Logo</a>
+        <ul className="navigation">  
+          <li><a href="#">Learn React</a></li>
+          <li><a href="#">Learn React</a></li>
+          <li><a href="#">Learn React</a></li>
+          <li><a href="#">Learn React</a></li>
+        </ul> 
       </header>
     </div>
   );
