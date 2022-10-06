a747a8c6379b0068a24acfec69bf9055d2e91f24
Juan Almeida <juanreboucas@gmail.com>
2022-10-06 09:15:14 -0300

    remodelacao em toda estrutura

--- fanpage/src/App.js
+++ fanpage/src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="#" className="logo">Logo</a>
-        <ul className="navigation">  
-          <li><a href="#">Learn React</a></li>
-          <li><a href="#">Learn React</a></li>
-          <li><a href="#">Learn React</a></li>
-          <li><a href="#">Learn React</a></li>
-        </ul> 
-      </header>
-      <section> 
-        <h2 id="text">Star Wars</h2>
-        <img src="starwars.png"></img>
-      </section>
+     <h1>hello</h1>
     </div>
   );
 }
